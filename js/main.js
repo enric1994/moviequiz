fc2a9a159fcd441b0d74a315194b9b801a3beb42
Enric Moreu <enric.moreu.filella@gmail.com>
2021-06-05 19:49:04 +0200

    Platform test

--- js/main.js
+++ js/main.js
@@ -6,97 +6,143 @@ let camera;
 
 init();
 animate();
-  
+
 function init() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    
+    width = 300;//window.innerWidth;
+    height = 300;//window.innerHeight;
+
     scene = new THREE.Scene();
-    
-    // let ambient = new THREE.AmbientLight( 0x101030 );
-    // scene.add( ambient );
 
-    const light = new THREE.SpotLight(0xFFFFFF, 2, 100, Math.PI / 4, 8);
-    light.position.set( 10, 25, 25 );
-    light.castShadow = true;
-    scene.add(light);
-    
-    camera = new THREE.PerspectiveCamera( 60, width / height, 0.01, 10000 );
-    //camera.position.set(1, 5, 30);
-    camera.position.set(40, 10, 30);
+    // Camera
+    camera = new THREE.PerspectiveCamera(60, width / height, 0.01, 10000);
+    camera.position.set(40, 30, 30);
 
-    // let geometry = new THREE.BoxGeometry(100, 5, 100);
-    // let material = new THREE.MeshLambertMaterial({
-    //     color: "0xffffff",
-    //     emissive: "0xffffff"
-    // });
-    
-    // let ground = new THREE.Mesh(geometry, material);
-    // ground.position.y -= 15;
-    // ground.receiveShadow = true;
-    // scene.add(ground);
+    // Background
+    scene.background = new THREE.Color().setHSL(0.6, 0, 1);
+    scene.fog = new THREE.Fog(scene.background, 1, 5000);
 
-    // let manager = new THREE.LoadingManager();
-    // manager.onProgress = function ( item, loaded, total ) {
-    //     console.log( item, loaded, total );
-    // };
+    // Lights
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+    hemiLight.color.setHSL(0.6, 1, 0.6);
+    hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+    hemiLight.position.set(0, 50, 0);
+    scene.add(hemiLight);
+
+    // const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
+    // scene.add(hemiLightHelper);
+
+    //
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    // dirLight.color.setHSL(0, 1, 0.95);
+    dirLight.position.set(- 1, 1.75, 1);
+    dirLight.position.multiplyScalar(30);
+    scene.add(dirLight);
+
+    dirLight.castShadow = true;
+
+    dirLight.shadow.mapSize.width = 2048;
+    dirLight.shadow.mapSize.height = 2048;
+
+    const d = 50;
+
+    dirLight.shadow.camera.left = - d;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = - d;
+
+    dirLight.shadow.camera.far = 3500;
+    dirLight.shadow.bias = - 0.0001;
+
+    const dirLight2 = new THREE.DirectionalLight(0xffffff, 1);
+    // dirLight2.color.setHSL(0, 1, 0.95);
+    dirLight2.position.set( 1, -1.75, -1);
+    dirLight2.position.multiplyScalar(30);
+    scene.add(dirLight2);
+
+    dirLight2.castShadow = true;
+
+    dirLight2.shadow.mapSize.width = 2048;
+    dirLight2.shadow.mapSize.height = 2048;
+
+    const d2 = -20;
+
+    dirLight2.shadow.camera.left = - d2;
+    dirLight2.shadow.camera.right = d2;
+    dirLight2.shadow.camera.top = d2;
+    dirLight2.shadow.camera.bottom = - d2;
+
+    dirLight2.shadow.camera.far = 3500;
+    dirLight2.shadow.bias = - 0.0001;
+
+    // const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 10);
+    // scene.add(dirLightHelper);
+
+    // Ground
+
+    // const plane = new THREE.Mesh(
+    //     new THREE.PlaneGeometry( 200, 200 ),
+    //     new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+    // );
+    // plane.rotation.x = - Math.PI / 2;
+    // plane.position.y = -15;
+    // scene.add( plane );
+
+    // plane.receiveShadow = true;
+
+    // 3D model
 
     let loader = new THREE.GLTFLoader();
-    loader.setCrossOrigin( 'anonymous' ); // r84 以降は明示的に setCrossOrigin() を指定する必要がある
 
-    let scale = 50.0;
-    // let url = "https://rawcdn.githack.com/BabylonJS/MeshesLibrary/55f475726670be2e7e4017b5f88c5762a90508c2/shark.glb";
-    let url = "gltf/platform.glb";
-    
+    let scale = 13.0;
+    let url = "gltf/platform6.glb";
+
     loader.load(url, function (data) {
         gltf = data;
         let object = gltf.scene;
         object.scale.set(scale, scale, scale);
-        object.position.y = -5;
-        object.position.x = 4;
         object.castShadow = true;
         object.receiveShadow = true;
 
         let animations = gltf.animations;
-        if ( animations && animations.length ) {
-            mixer = new THREE.AnimationMixer( object );
-            for ( let i = 0; i < animations.length; i ++ ) {
-                let animation = animations[ i ];
-                mixer.clipAction( animation ).play();
+        if (animations && animations.length) {
+            mixer = new THREE.AnimationMixer(object);
+            for (let i = 0; i < animations.length; i++) {
+                let animation = animations[i];
+                mixer.clipAction(animation).play();
             }
         }
         scene.add(object);
     });
 
-    let axis = new THREE.AxesHelper(1000);   
-    scene.add(axis);
+    // let axis = new THREE.AxesHelper(1000);
+    // scene.add(axis);
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor( 0xbfe4ff );
-    // renderer.setClearColor( 0x000000 );
     renderer.shadowMap.enabled = true;
-    
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.userPan = false;
+
+    // Orbit Controls
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableZoom = false;
     controls.userPanSpeed = 0.0;
     controls.maxDistance = 5000.0;
-    controls.maxPolarAngle = Math.PI * 0.495;
-    //controls.autoRotate = true;
+    // controls.maxPolarAngle = Math.PI * 0.30;
+    // controls.minPolarAngle = Math.PI * 0.30;
     controls.autoRotate = false;
     controls.autoRotateSpeed = -2.0;
 
-    renderer.setSize( width, height );
+    renderer.setSize(width, height);
     renderer.gammaOutput = true;
-    document.body.appendChild( renderer.domElement );
+    document.body.appendChild(renderer.domElement);
 }
 
 function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     if (mixer) mixer.update(clock.getDelta());
     controls.update();
     render();
 }
 
 function render() {
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
