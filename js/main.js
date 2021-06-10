71344fa204525f8f950456504bd7c196d846ec3c
Enric Moreu <enric.moreu.filella@gmail.com>
2021-06-10 12:35:54 +0200

    Format + new animation steps

--- js/main.js
+++ js/main.js
@@ -1,120 +1,122 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
-import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
-import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
 
 
-  let container;
-  let camera;
-  let renderer;
-  let scene;
-  let controls;
-  
-  const mixers = [];
-  const clock = new THREE.Clock();
-  
-  function init() {
-    container = document.querySelector("#scene-container");
-  
-    // Creating the scene
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color("#7abcff");
-  
-    createCamera();
-    createLights();
-    loadModels();
-    createControls();
-    createRenderer();
-  
-    renderer.setAnimationLoop(() => {
-      update();
-      render();
-    });
-  }
-  
-  function createCamera() {
-    const fov = 60;
-    const aspect = container.clientWidth / container.clientHeight;
-    const near = 0.01;
-    const far = 10000;
-    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(4, 9, 12);
-  }
-  
-  function createLights() {
-    const mainLight = new THREE.DirectionalLight(0xffffff, 5);
-    mainLight.position.set(10, 10, 10);
-  
-    const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x202020, 5);
-    scene.add(mainLight, hemisphereLight);
-  }
-  
-  function loadModels() {
-    const loader = new GLTFLoader();
-  
-    const onLoad = (result, position) => {
-      const model = result.scene;
-      // model.position.copy(position);
-      model.scale.set(3,3,3);
-  
-      const mixer = new THREE.AnimationMixer(model);
-      mixers.push(mixer);
+let container;
+let camera;
+let renderer;
+let scene;
+let controls;
 
-      // console.log(result.animations.length);
-      var i;
-      for (i = 0; i < result.animations.length; i++) {
+const mixers = [];
+const clock = new THREE.Clock();
+
+function init() {
+  container = document.querySelector("#scene-container");
+
+  // Creating the scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color("#7abcff");
+
+  createCamera();
+  createLights();
+  loadModels();
+  createControls();
+  createRenderer();
+
+  renderer.setAnimationLoop(() => {
+    update();
+    render();
+  });
+}
+
+function createCamera() {
+  const fov = 60;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.01;
+  const far = 10000;
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(4, 9, 12);
+}
+
+function createLights() {
+  const mainLight = new THREE.DirectionalLight(0xffffff, 5);
+  mainLight.position.set(10, 10, 10);
+
+  const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x202020, 5);
+  scene.add(mainLight, hemisphereLight);
+}
+
+function loadModels() {
+  const loader = new GLTFLoader();
+
+  const onLoad = (result, position) => {
+    const model = result.scene;
+    // model.position.copy(position);
+    model.scale.set(3, 3, 3);
+
+    const mixer = new THREE.AnimationMixer(model);
+    mixers.push(mixer);
+
+    // console.log(result.animations.length);
+    var i;
+    for (i = 0; i < result.animations.length; i++) {
 
       const animation = result.animations[i];
       const action = mixer.clipAction(animation);
       action.play();
-      }
-  
-      scene.add(model);
-    };
-  
-    const onProgress = (progress) => {};
-  
-    const parrotPosition = new THREE.Vector3(0, 0, 2.5);
-    loader.load(
-      "gltf/steps2.glb",
-      (gltf) => onLoad(gltf, parrotPosition),
-      onProgress
-    );
-  
-  }
-  
-  function createRenderer() {
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.gammaFactor = 2.2;
-    // renderer.gammaOutput = true;
-    renderer.physicallyCorrectLights = true;
-  
-    container.appendChild(renderer.domElement);
-  }
-  
-  function createControls() {
-    controls = new OrbitControls(camera, container);
-  }
-  
-  function update() {
-    const delta = clock.getDelta();
-    mixers.forEach((mixer) => mixer.update(delta));
-  }
-  
-  function render() {
-    renderer.render(scene, camera);
-  }
-  
-  init();
-  
-  function onWindowResize() {
-    camera.aspect = container.clientWidth / container.clientHeight;
-  
-    // Update camera frustum
-    camera.updateProjectionMatrix();
-  
-    renderer.setSize(container.clientWidth, container.clientHeight);
-  }
-  window.addEventListener("resize", onWindowResize, false);
-  
+    }
+
+    scene.add(model);
+  };
+
+  const onProgress = (progress) => { };
+
+  const parrotPosition = new THREE.Vector3(0, 0, 2.5);
+  loader.load(
+    "gltf/steps2.glb",
+    (gltf) => onLoad(gltf, parrotPosition),
+    onProgress
+  );
+
+}
+
+function createRenderer() {
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.gammaFactor = 2.2;
+  // renderer.gammaOutput = true;
+  renderer.physicallyCorrectLights = true;
+
+  container.appendChild(renderer.domElement);
+}
+
+function createControls() {
+  controls = new OrbitControls(camera, container);
+  controls.enableZoom = false;
+  controls.maxPolarAngle = 1;
+  controls.minPolarAngle = 1;
+}
+
+function update() {
+  const delta = clock.getDelta();
+  mixers.forEach((mixer) => mixer.update(delta));
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight;
+
+  // Update camera frustum
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+window.addEventListener("resize", onWindowResize, false);
